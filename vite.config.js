1b5de736f1a82fef6e1cbff2b846ec8d1f4cc9b8
bran-can19 <Candanedabrandon@gmail.com>
2026-06-27 16:16:38 -0600

    areglos vite

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,15 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/', // <--- ¡AGREGA ESTA LÍNEA AQUÍ!
+  base: "./", // Fuerza rutas relativas para que funcione en GitHub Pages
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    extensions: [".js", ".jsx", ".json"], // Asegura la resolución de extensiones
+  },
+  esbuild: {
+    loader: "jsx", // Fuerza al compilador a procesar la sintaxis JSX de forma estricta
+    include: /src\/.*\.jsx?$/, 
   },
 });
